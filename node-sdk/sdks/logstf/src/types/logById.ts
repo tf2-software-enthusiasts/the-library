ad95df33f48c0be172a37f99209a5f40a6aa206c
c43721 <rlahman.game@gmail.com>
2022-12-22 19:25:23 -0600

    feat(node-sdk): finish log by id types

--- node-sdk/sdks/logstf/src/types/logById.ts
+++ node-sdk/sdks/logstf/src/types/logById.ts
@@ -1,3 +1,8 @@
+enum TeamType {
+  Red,
+  Blue,
+}
+
 type TeamInformation = {
   score: number;
   kills: number;
@@ -21,7 +26,7 @@ type MedicStats = {
 };
 
 type PlayerInformation = {
-  team: "Red" | "Blue";
+  team: TeamType;
   kills: number;
   deaths: number;
   assists: number;
@@ -73,6 +78,32 @@ type ClassStats = {
   total_time: number;
 };
 
+type TeamRoundsInfo = {
+  score: number;
+  kills: number;
+  dmg: number;
+  ubers: number;
+};
+
+type RoundEvent = {
+  type: string;
+  time: number;
+  team: TeamType;
+  point: number;
+};
+
+type Tf2ClassMap = {
+  scout?: number;
+  solider?: number;
+  pyro?: number;
+  demoman?: number;
+  heavyweapons?: number;
+  engineer?: number;
+  medic?: number;
+  sniper?: number;
+  spy?: number;
+};
+
 export type LogById = {
   version: string;
   length: number;
@@ -84,12 +115,78 @@ export type LogById = {
   names: {
     [steamIdThree: string]: string;
   };
-  rounds: [];
-  healspread: {};
-  classkills: {};
-  classdeaths: {};
-  classkillassists: {};
-  chat: [];
-  info: {};
-  killstreaks: [];
+  rounds: [
+    {
+      start_time: number;
+      winner: TeamType;
+      team: {
+        Blue: TeamRoundsInfo;
+        Red: TeamRoundsInfo;
+      };
+      events: RoundEvent[];
+      players: {
+        [steamIdThree: string]: {
+          kills: number;
+          dmg: number;
+        };
+      };
+      firstcap: TeamType;
+      length: number;
+    }
+  ];
+  healspread: {
+    [steamIdThree: string]: {
+      [steamIdThree: string]: number;
+    };
+  };
+  classkills: {
+    [steamIdThree: string]: Tf2ClassMap;
+  };
+  classdeaths: {
+    [steamIdThree: string]: Tf2ClassMap;
+  };
+  classkillassists: {
+    [steamIdThree: string]: Tf2ClassMap;
+  };
+  chat: [
+    {
+      steamId: string;
+      name: string;
+      msg: string;
+    }
+  ];
+  info: {
+    map: "cp_orange_x3";
+    supplemental: boolean;
+    total_length: number;
+    hasRealDamage: boolean;
+    hasWeaponDamage: boolean;
+    hasAccuracy: boolean;
+    hasHP: boolean;
+    hasHP_real: boolean;
+    hasHS: boolean;
+    hasHS_hit: boolean;
+    hasBS: boolean;
+    hasCP: boolean;
+    hasSB: boolean;
+    hasDT: boolean;
+    hasAS: boolean;
+    hasHR: boolean;
+    hasIntel: boolean;
+    notifications: [];
+    title: string;
+    date: number;
+    uploader: {
+      id: string;
+      name: string;
+      info: string;
+    };
+  };
+  killstreaks: [
+    {
+      steamid: string;
+      streak: number;
+      time: number;
+    }
+  ];
 };
