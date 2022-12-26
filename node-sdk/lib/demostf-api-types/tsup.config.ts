a97d2ca209fd668780178eed493639ce932a072a
c43721 <rlahman.game@gmail.com>
2022-12-26 17:27:05 -0600

    feat(sdk,lib): add demostf

---
+++ node-sdk/lib/demostf-api-types/tsup.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  clean: false,
+  bundle: false,
+  dts: true,
+  entry: ["src/**/*.ts", "!src/**/*.d.ts"],
+  format: ["esm", "cjs"],
+  minify: false,
+  tsconfig: "tsconfig.json",
+  target: "esnext",
+  splitting: false,
+  skipNodeModulesBundle: true,
+  sourcemap: true,
+  shims: false,
+  keepNames: true,
+});
