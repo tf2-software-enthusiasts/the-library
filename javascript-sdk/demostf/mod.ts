063e4f258fdc5825874e67cc4a99065b01d9d71c
c43721 <rlahman.game@gmail.com>
2024-11-08 12:44:10 -0600

    chore: denoify

---
+++ javascript-sdk/demostf/mod.ts
@@ -0,0 +1,10 @@
+/**
+ * The [demos.tf](https://demos.tf/about) SDK
+ *
+ * This API provides both the types and methods to interact with the API
+ *
+ * @module
+ */
+
+export * from "./types/mod.ts";
+export * from "./api/mod.ts";
