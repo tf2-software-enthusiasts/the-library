1c590a7a53804975f0f43224f0966abacd6ab765
c43721 <rlahman.game@gmail.com>
2024-11-08 14:35:11 -0600

    chore(types): finish demo types

--- javascript-sdk/demostf/types/demo.ts
+++ javascript-sdk/demostf/types/demo.ts
@@ -68,4 +68,163 @@ export type Demo = {
    * The unique id of the user who uploaded the demo
    */
   uploader: number;
+
+  /**
+   * Gets the hash of the file
+   */
+  hash: string;
+
+  /**
+   * Gets the backend the file is stored on
+   */
+  backend: string;
+
+  /**
+   * Gets the path of the file
+   */
+  path: string;
+};
+
+export type DemoInfo = {
+  /**
+   * The id of the demo
+   */
+  id: number;
+
+  /**
+   * The URL of the demo
+   */
+  url: string;
+
+  /**
+   * The name of the file
+   */
+  name: string;
+
+  /**
+   * The hostname of the server that uploaded the demo
+   */
+  server: string;
+
+  /**
+   * The duration in ticks of the demo
+   */
+  duration: 1809;
+
+  /**
+   * The nickname of Source TV
+   */
+  nick: string;
+
+  /**
+   * The map that was played
+   */
+  map: string;
+
+  /**
+   * The date the demo was uploaded
+   */
+  time: number;
+
+  /**
+   * The name of the RED team
+   */
+  red: string;
+
+  /**
+   * The name of the BLU team
+   */
+  blue: string;
+
+  /**
+   * The score of the RED team
+   */
+  redScore: number;
+
+  /**
+   * The name of the BLU team
+   */
+  blueScore: number;
+
+  /**
+   * The number of players in the demo
+   */
+  playerCount: number;
+
+  /**
+   * The information about the uploader
+   */
+  uploader: {
+    /**
+     * The id of the uploader
+     */
+    id: number;
+
+    /**
+     * The steam id of the uploader
+     */
+    steamid: string;
+
+    /**
+     * The name of the uploader
+     */
+    name: string;
+  };
+
+  /**
+   * The information about each player in the demo
+   */
+  players: [
+    {
+      /**
+       * The id of the player
+       */
+      id: number;
+
+      /**
+       * The user id of the player
+       */
+      user_id: number;
+
+      /**
+       * The name of the player
+       */
+      name: string;
+
+      /**
+       * The team of the player
+       */
+      team: string;
+
+      /**
+       * The class the player played on
+       */
+      class: string;
+
+      /**
+       * The steam id of the user
+       */
+      steamid: string;
+
+      /**
+       * The avatar URL of the player
+       */
+      avatar: string;
+
+      /**
+       * The number of kills the player had
+       */
+      kills: number;
+
+      /**
+       * The number of assists
+       */
+      assists: number;
+
+      /**
+       * The number of times the player died
+       */
+      deaths: number;
+    }
+  ];
 };
