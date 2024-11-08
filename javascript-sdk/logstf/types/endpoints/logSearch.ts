6c66d1b870d1fad0345dd8aa7f49f05020104c9b
c43721 <rlahman.game@gmail.com>
2024-11-08 12:12:39 -0600

    chore(docs): add documentation, fix labeler

--- javascript-sdk/logstf/types/endpoints/logSearch.ts
+++ javascript-sdk/logstf/types/endpoints/logSearch.ts
@@ -1,10 +1,30 @@
 import type { LogSearchRequest } from "./logSearchRequest.ts";
 
+/**
+ * The result of a search request
+ */
 export type LogSearchResponse = {
+  /**
+   * A flag that determines the success of the operation
+   */
   success: boolean;
+  /**
+   * The number of results
+   */
   results: number;
+
+  /**
+   * The total number of results
+   */
   total: number;
+  /**
+   * The parameters used in the search request
+   */
   parameters: LogSearchRequest;
+
+  /**
+   * The results
+   */
   logs: [
     {
       id: number;
