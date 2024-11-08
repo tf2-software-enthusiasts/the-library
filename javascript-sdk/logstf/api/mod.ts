6fe671b211f5329a8328fdc04b22c57e34b21b74
c43721 <rlahman.game@gmail.com>
2024-11-08 13:49:43 -0600

    chore: module docs

--- javascript-sdk/logstf/api/mod.ts
+++ javascript-sdk/logstf/api/mod.ts
@@ -1,6 +1,12 @@
 /**
  * Provides methods used to interact with the [logs.tf api](https://logs.tf/about)
  *
+ * @example Fetching the JSON for a log
+ * ```ts
+ * const logJson = await getById("3750052");
+ *
+ * console.log(logJson);
+ * ```
  * @module
  */
 
